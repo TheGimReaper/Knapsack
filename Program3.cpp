21b633c7a9f6b65821a99c7b774d54ba8829fe27
Alison Gim <alisonthgim@gmail.com>
2018-10-29 22:49:12 -0400

    clock clock

--- Program3.cpp
+++ Program3.cpp
@@ -16,7 +16,6 @@ int main(int argc, char** argv)
     {
         clock_t start, end;
 		start = clock();
-        //cout << start << endl;
         Knapsack sack(capacity, num_items);
         int weight, profit;
         for (int i = 0; i < num_items; i++)
@@ -27,29 +26,13 @@ int main(int argc, char** argv)
         }
         vector<Card*> include;
         output << num_items << " ";
-        //sack.print_card_list2();
         sack.quick_sort(sack.get_card_list(), 0, sack.get_num_cards() - 1);
-        //sack.print_card_list();
-        //sack.print_card_list2();
         if (mode == "0") sack.greedy();
         else sack.better_greedy();
         if (mode == "2") sack.backtracking(0, 0, 0);
-        
-        for (int i = 0; i < 20000; i++)
-        {
-            for (int j = 0; j < 20000; j++)
-            {
-                int k = i * j;
-            }
-        }
-        
         output << sack.get_max_profit() << " ";
         end = clock();
-        //cout << CLOCKS_PER_SEC << endl;
-        //cout << end << endl;
-        //cout << start << endl;
         double duration = (double) (end - start)/ (CLOCKS_PER_SEC);
-        //duration =  ((double) (end - start)/(double) (CLOCKS_PER_SEC));
 		output << duration << "\n";
     }
     input.close();
